8fe952ff2ed051ead402106f655470b7605458e6
snowmerak <snowmerak@outlook.com>
2025-01-19 17:51:06 +0900

    add os.Kill to running app when stop

--- run_go.go
+++ run_go.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/rs/zerolog/log"
 )
 
 func RunGoApp(ctx context.Context, path string) error {
@@ -14,12 +16,17 @@ func RunGoApp(ctx context.Context, path string) error {
 	cmd.Stderr = os.Stderr
 
 	context.AfterFunc(ctx, func() {
+		cmd.Process.Signal(os.Interrupt)
+		cmd.Process.Signal(os.Kill)
 		cmd.Process.Kill()
 	})
 
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("stopped app: %w", err)
 	}
+
+	cmd.Process.Wait()
+	log.Info().Msg("app stopped")
 
 	return nil
 }
