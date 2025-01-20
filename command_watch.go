7505c681e0c551df021a930984b64571f0931abb
snowmerak <snowmerak@outlook.com>
2025-01-21 00:51:41 +0900

    WIP: make command watch

---
+++ command_watch.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v3"
+)
+
+const CommandWatch = "watch"
+
+var CommandWatchNotFoundFilenameError = fmt.Errorf("missing filename to watch")
+
+func CommandWatchFunc(args []string) error {
+	if len(args) < 1 {
+		fmt.Printf("Usage: %s %s <filename>\n", os.Args[0], CommandWatch)
+		return CommandWatchNotFoundFilenameError
+	}
+
+	filename := args[0]
+
+	fmt.Printf("Watching file: %s\n", filename)
+
+	cfg := RevolverConfig{}
+	file, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&cfg); err != nil {
+		return fmt.Errorf("failed to decode yaml: %w", err)
+	}
+
+	log.Info().Str("filename", filename).Any("config", cfg).Msg("watching with config")
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	wc, err := NewWatcher(ctx, WithPath(cfg.ProjectRootFolder), WithExtensionFilter(cfg.ObservingExts...))
+	if err != nil {
+		return fmt.Errorf("failed to create watcher: %w", err)
+	}
+
+	rpm := map[string]*TcpReverseProxy{}
+	for _, port := range cfg.Ports {
+		rp := NewTcpReverseProxy("0.0.0.0:" + strconv.FormatInt(int64(port.Port), 10))
+		go func() {
+			if err := rp.Start(ctx); err != nil {
+				log.Error().Err(err).Msg("failed to start reverse proxy")
+				panic("occurred critical error!!")
+			}
+		}()
+		rpm[port.Name] = rp
+	}
+
+	wc.AddEventHandler("log", func(event *fsnotify.Event) {
+		log.Info().Str("event_data", event.String()).Msg("event received")
+	})
+
+	wc.AddEventHandler("restart", func(event *fsnotify.Event) {
+		ctx, cancel := context.WithCancel(ctx)
+
+		id, err := NewSession()
+		if err != nil {
+			cancel()
+			log.Error().Err(err).Msg("failed to create new session")
+			return
+		}
+
+		portMap, err := GetFreeTcpPortEnv(cfg.Ports)
+		if err != nil {
+			cancel()
+			log.Error().Err(err).Msg("failed to get free tcp port")
+			return
+		}
+
+		for name, rp := range rpm {
+			if err := rp.RenewDestination(id, "0.0.0.0:"+strconv.FormatInt(int64(portMap[name]), 10), func() {
+				cancel()
+			}); err != nil {
+				cancel()
+				log.Error().Err(err).Msg("failed to renew destination")
+				return
+			}
+		}
+
+		// TODO: implement the rest of the logic here
+	})
+
+	return nil
+}
