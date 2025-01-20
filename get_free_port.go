a047ff45b2e8e8eb3d46470b2ade4a32ea9d00db
snowmerak <snowmerak@outlook.com>
2025-01-21 00:45:43 +0900

    make get free tcp port env

--- get_free_port.go
+++ get_free_port.go
@@ -1,6 +1,8 @@
 package main
 
-import "net"
+import (
+	"net"
+)
 
 func GetFreeTcpPort() (int, error) {
 	conn, err := net.ListenTCP("tcp", &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 0})
@@ -10,4 +12,18 @@ func GetFreeTcpPort() (int, error) {
 	defer conn.Close()
 
 	return conn.Addr().(*net.TCPAddr).Port, nil
+}
+
+func GetFreeTcpPortEnv(portSet []RevolverPortConfig) (map[string]int, error) {
+	env := make(map[string]int, len(portSet))
+	for _, port := range portSet {
+		freePort, err := GetFreeTcpPort()
+		if err != nil {
+			return nil, err
+		}
+
+		env[port.Env] = freePort
+	}
+
+	return env, nil
 }
